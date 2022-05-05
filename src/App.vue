dc5fc0fb78111007b543bb671bec5eccf976f168
AldairFloresCayetano <89316165+AldairFloresCayetano@users.noreply.github.com>
2022-05-04 21:43:46 -0500

    chore: add navigation routes

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
 export default {
   data() {
     return {
-
+      drawer: false,
       items: [
         { label: "Profile", to: "/profiles" },
-        { label: "Advisory", to: "/advisoryes" }
+        { label: "Advisory", to: "/advisoryes" },
       ],
     };
   },
@@ -15,47 +15,22 @@ export default {
 <template>
   <pv-toast></pv-toast>
   <header>
-    <pv-toolbar class="toolbar bg-primary">
+
+    <pv-toolbar class="bg-primary">
       <template #start>
-        <img
-          alt="Getting Better logo"
-          class="logo"
-          src="@/assets/logo-gb.png"
-          width="160"
-          height="125"
-        />
+        <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="drawer=!drawer"></pv-button>
+        <h3>ACME Learning Center</h3>
       </template>
       <template #end>
         <div class="flex-column">
-          <router-link
-            v-for="item in items"
-            :to="item.to"
-            custom
-            v-slot="{ navigate, href }"
-            :key="item.label"
-
-          >
-            <pv-button
-              class="p-button"
-              :href="href"
-              @clcik="navigate"
-              >{{ item.label }}</pv-button
-            >
+          <router-link v-for="item in items" :to="item.to" custom v-slot="{ navigate, href }" :key="item.label">
+            <pv-button class="p-button-text text-white" :href="href" @click="navigate">{{ item.label }}</pv-button>
           </router-link>
         </div>
       </template>
     </pv-toolbar>
+
   </header>
   <pv-sidebar v-model:visible="drawer"></pv-sidebar>
   <RouterView />
 </template>
-
-<style scoped>
-.toolbar{
-  background-color: #000000;
-}
-.p-button{
-  color: #ffffff;
-  background-color: #000000;
-}
-</style>
